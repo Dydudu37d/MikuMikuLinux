cdcdf148f76f4015a6a3f7b855b2031ee437cafd
Dydudu37d <lifuan666@gamil.com>
2026-06-04 21:26:12 +0800

    What can i say,manba out

--- Fish3D.h
+++ Fish3D.h
@@ -3,28 +3,26 @@
 #include <stdint.h>
 #include <math.h>
 
-typedef struct {int64_t x,y,z;} Pos3D;
-typedef struct {int64_t x,y,z;} Scale3D;
-typedef struct {int64_t x[3],y[3],z[3];} FaceTriangle;
-typedef struct {int64_t x[4],y[4],z[4];} FaceSquare;
-typedef struct {Pos3D Pos;FaceSquare Faces[6];} Square3D;
-typedef struct {Pos3D Pos;FaceTriangle Faces[4];} Triangle3D;
-typedef struct {Pos3D Pos;uint64_t FaceCount;FaceTriangle Faces[];} Polygon3D;
+typedef struct {int64_t x,y,z;} __attribute__((aligned(16))) Pos3D;
+typedef struct {int64_t x,y,z;} __attribute__((aligned(16))) Scale3D;
+typedef struct {int64_t x[3],y[3],z[3];} __attribute__((aligned(16))) FaceTriangle;
+typedef struct {int64_t x[4],y[4],z[4];} __attribute__((aligned(16))) FaceSquare;
+typedef struct {Pos3D Pos;int64_t x[24],y[24],z[24];} __attribute__((aligned(16))) Square3D;
+typedef struct {Pos3D Pos;int64_t x[12],y[12],z[12];} __attribute__((aligned(16))) Triangle3D;
+typedef struct {Pos3D Pos;uint64_t FaceCount;FaceTriangle Faces[];} __attribute__((aligned(16))) Polygon3D;
 
 #define SIGNED_TO_ORDERED_UINT64(i64) ((uint64_t)(i64) ^ 0x8000000000000000ULL)
-#define PROJECTION_X(x, z, Screen_W,FOCAL_LENGTH) (z ? ((double)(x) * FOCAL_LENGTH) / (double)(z) + ((double)(Screen_W) / 2.0f):0 )
-#define PROJECTION_Y(y, z, Screen_H,FOCAL_LENGTH) (z ? ((double)(y) * FOCAL_LENGTH) / (double)(z) + ((double)(Screen_H) / 2.0f):0 )
 
-#define CreateTriangle(x,y,z) ((Triangle3D){(Pos3D){x,y,z},\
-	{(FaceTriangle){{0,1,0},{0,0,0},{0,0,1}},(FaceTriangle){{0,0,0},{0,1,0},{0,0,1}},\
-	(FaceTriangle){{0,0,1},{0,1,0},{1,0,0}},(FaceTriangle){{0,0,1},{0,1,0},{0,0,0}}}})
+#define PROJECTION_X(local_x, world_pos_x, z, Screen_W, FOCAL_LENGTH) \
+    (z ? (int)( (((double)(local_x) - (local_x ? local_x/2 : 0)) * FOCAL_LENGTH) / (double)(z) + (double)(world_pos_x) + ((double)(Screen_W) / 2.0f) ) : 0)
 
-#define CreateSquare(x,y,z) ((Square3D){(Pos3D){x,y,z},{\
-	(FaceSquare){{0,1,1,0},{0,0,0,0},{0,0,1,1}},(FaceSquare){{0,0,0,0},{0,1,1,0},{0,0,1,1}},\
-	(FaceSquare){{0,1,1,0},{0,0,1,1},{1,1,1,1}},(FaceSquare){{0,1,1,0},{0,0,1,1},{0,0,0,0}},\
-	(FaceSquare){{0,1,1,0},{1,1,1,1},{0,0,1,1}},(FaceSquare){{1,1,1,1},{0,1,1,0},{0,0,1,1}}}})
+#define PROJECTION_Y(local_y, world_pos_y, z, Screen_H, FOCAL_LENGTH) \
+    (z ? (int)( (((double)(local_y) - (local_y ? local_y/2 : 0)) * FOCAL_LENGTH) / (double)(z) + (double)(world_pos_y) + ((double)(Screen_H) / 2.0f) ) : 0)
 
-#define CreatePolygon(x,y,z,Faces) ((Polygon3D){(Pos3D){x,y,z},Faces})
+
+#define CreateTriangle(x,y,z) ((Triangle3D){(Pos3D){x,y,z},{0,1,0, 0,0,0, 0,0,1, 0,0,1},{0,0,0, 0,1,0, 0,1,0, 0,1,0},{0,0,1, 0,0,1, 1,0,0, 0,0,0}})
+
+#define CreateSquare(x,y,z) ((Square3D){(Pos3D){x,y,z},{0,1,1,0, 0,0,0,0, 0,1,1,0, 0,1,1,0, 0,1,1,0, 1,1,1,1},{0,0,0,0, 0,1,1,0, 0,0,1,1, 0,0,1,1, 1,1,1,1, 0,1,1,0},{0,0,1,1, 0,1,1,0, 1,1,1,1, 0,0,0,0, 0,0,1,1, 0,0,1,1}})
 
 #define RotateX(x,y,z,c,s) \
 	do {\
--- Main.c
+++ Main.c
@@ -1,36 +1,83 @@
 #include "Fish3D.h"
+#include <SDL2/SDL_error.h>
 #include <SDL2/SDL_events.h>
+#include <SDL2/SDL_keyboard.h>
+#include <SDL2/SDL_keycode.h>
+#include <SDL2/SDL_render.h>
 #include <SDL2/SDL_video.h>
+#include <SDL2/SDL_rect.h>
 #include <stdint.h>
-#include <stdio.h>
 #include <SDL2/SDL.h>
 
-int main(){
-	printf("Start\n");
-	SDL_DisplayMode dm;
-	int Dw=1000,Dh=1000;
-	if (SDL_GetDesktopDisplayMode(0, &dm)){
-		Dw=dm.w;
-		Dh=dm.h;
+void DrawSquare3D(SDL_Renderer *Render,Square3D Obj,int ScreenW,int ScreenH,uint8_t r,uint8_t g,uint8_t b,uint8_t a,int Pov){
+	if (Obj.Pos.z == 0) return;
+	SDL_SetRenderDrawColor(Render, r, g, b, a);
+	Pos3D Pos=Obj.Pos;
+	for (int i = 0; i < 24; i++) {
+		if (Pos.z + Obj.z[i] <= 0) continue;
+		int next_idx = (i & ~3) + ((i + 1) & 3); 
+		if (Pos.z + Obj.z[next_idx] <= 0) continue;
+		int x1 = PROJECTION_X(Obj.x[i],        Pos.x, Pos.z + Obj.z[i],        ScreenW, Pov);
+		int y1 = PROJECTION_Y(Obj.y[i],        Pos.y, Pos.z + Obj.z[i],        ScreenH, Pov);
+		int x2 = PROJECTION_X(Obj.x[next_idx], Pos.x, Pos.z + Obj.z[next_idx], ScreenW, Pov);
+		int y2 = PROJECTION_Y(Obj.y[next_idx], Pos.y, Pos.z + Obj.z[next_idx], ScreenH, Pov);
+		SDL_RenderDrawLine(Render, x1, y1, x2, y2);
 	}
-	SDL_Window *Window=SDL_CreateWindow("",Dw/4,Dh/4,Dw/2,Dh/2,SDL_WINDOW_SHOWN);
-	_Bool running=1;
-	SDL_Event Event;
-	Square3D Cube=CreateSquare(10, 10, 10);
+}
 
+int main(){
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL Init Failed: %s\n", SDL_GetError());
+		return -1;
+	}
+	SDL_DisplayMode dm;
+    
+	if (SDL_GetDesktopDisplayMode(0, &dm) != 0) {
+		printf("Get mode failed: %s\n", SDL_GetError());
+		SDL_Quit();
+		return -1;
+	}
+
+	int ScreenW = dm.w;
+	int ScreenH = dm.h;
+	
+	SDL_Window* Window = SDL_CreateWindow("MikuMikuLinux", ScreenW/4, ScreenH/4, ScreenW/2, ScreenH/2, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+	if (!Window){
+		printf("Window Error: %s\n",SDL_GetError());
+		SDL_Quit();
+		return -1;
+	}
+	SDL_Renderer* Render = SDL_CreateRenderer(Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (!Render){
+		printf("Render Error: %s\n",SDL_GetError());
+		SDL_DestroyWindow(Window);
+		SDL_Quit();
+		return -1;
+	}
+	int WindowW,WindowH;
+	_Bool running=1;
+	Square3D Cube=CreateSquare(10, 10, 10);
+	SDL_Event Event;
 	while (running){
+		SDL_GetWindowSize(Window, &WindowW, &WindowH);
+		SDL_SetRenderDrawColor(Render, 0, 0, 0, 255);
+		SDL_RenderClear(Render);
 		while(SDL_PollEvent(&Event)){
-			if (Event.type == SDL_QUIT){
-				running=0;
-			}
-			if (Event.type == SDL_KEYDOWN){
-				// if(Event.key.keysym.sym == SDLK_XXXX){}
-			}
+			if (Event.type == SDL_QUIT) running=0;
 		}
+		const uint8_t* KeyState = SDL_GetKeyboardState(NULL);
 		
+		Cube.Pos.x+=(KeyState[SDL_SCANCODE_D]-KeyState[SDL_SCANCODE_A])*10;
+		Cube.Pos.y+=(KeyState[SDL_SCANCODE_E]-KeyState[SDL_SCANCODE_Q])*10;
+		Cube.Pos.z+=(KeyState[SDL_SCANCODE_W]-KeyState[SDL_SCANCODE_S])* 1;
+
+		DrawSquare3D(Render, Cube, WindowW, WindowH, 255, 255, 255, 255,1000);
+
+		SDL_RenderPresent(Render);
 		SDL_Delay(16);
 	}
 
+	SDL_DestroyRenderer(Render);
 	SDL_DestroyWindow(Window);
 	SDL_Quit();
 	return 0;
